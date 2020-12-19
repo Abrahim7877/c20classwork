c3acd678ff72bc252cc433cd53796d7462ff41ba
Abrahim7877 <75722795+Abrahim7877@users.noreply.github.com>
2020-12-20 10:56:17 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var fixedRect, movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+
+  fixedRect= createSprite(200,200,50,80);
+  fixedRect.shapeColor= "green";
+
+  movingRect= createSprite(500,300,80,40);
+  movingRect.shapeColor= "yellow";
+
+  movingRect.debug= true;
+  fixedRect.debug= true;
+
+
+}
+
+function draw() {
+  background(0);
+  console.log(movingRect.x-fixedRect.x);
+
+  movingRect.x= mouseX;
+  movingRect.y= mouseY;
+
+  if(movingRect.x-fixedRect.x< movingRect.width/2+fixedRect.width/2 &&
+    fixedRect.x-movingRect.x< movingRect.width/2+fixedRect.width/2 &&
+    fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2
+    && movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2){
+    movingRect.shapeColor= "red";
+  }
+  else{
+    movingRect.shapeColor= "yellow";
+  }
+
+  drawSprites();
+}
